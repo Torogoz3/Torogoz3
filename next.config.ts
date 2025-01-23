046200c117cfa66fe07417ea8bce4b93ad5f6f06
Chinaskidev <chinaskicharles0@gmail.com>
2025-01-22 18:01:53 -0600

    actualizaciones nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export', // Genera un sitio estático
+const nextConfig: NextConfig = {  
   basePath: '/Torogoz3', // Reemplaza con el nombre de tu repositorio
+  output: 'export', // Genera un sitio estático
   images: {
     unoptimized: true, // Necesario si usas imágenes en Next.js
   },

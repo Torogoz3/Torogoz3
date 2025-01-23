8d1b110de89aebf5d16f71bf17a951e507d18dbc
Chinaskidev <chinaskicharles0@gmail.com>
2025-01-22 21:00:38 -0600

    nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,7 @@ import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["example.com"], // Cambia esto por los dominios que uses para imágenes externas
-  },
+
 };
 
 export default nextConfig;

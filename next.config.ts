463a85a2d2fc11fbc364bac426b857ac195a11d3
Chinaskidev <chinaskicharles0@gmail.com>
2025-01-22 19:45:13 -0600

    nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Modo estricto para encontrar problemas potenciales
+  reactStrictMode: true,
+
+  // Configuración para el soporte de imágenes.
+  images: {
+    domains: ["example.com"], 
+    unoptimized: false, // Cambiar a true solo si se exporta a estatico
+  },
+
+  
+  webpack: (config) => {
+    
+    return config;
+  },
+
+  // Internacionalización
+  i18n: {
+    locales: ["en", "es"], // Idiomas soportados
+    defaultLocale: "en", // Idioma por defecto
+  },
+
 };
 
 export default nextConfig;

0dc638792d8f00e3f1db02dce9bbaeaab26ca1fa
Chinaskidev <chinaskicharles0@gmail.com>
2025-01-24 09:55:16 -0600

    Removed out folder

--- out/_next/static/chunks/app/onboard/layout-181beb0ca74455e6.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[649,954],{22533:(e,s,_)=>{Promise.resolve().then(_.bind(_,45410))}},e=>{var s=s=>e(e.s=s);e.O(0,[952,818,444,410,441,517,358],()=>s(22533)),_N_E=e.O()}]);
